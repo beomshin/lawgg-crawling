8dc90ba73245fb7ae46fb6afdb8a1351648379ea
beomshin.kim <chzh859@naver.com>
2024-08-24 23:11:03 +0900

    Initial commit

---
+++ src/main/java/com/kr/lg/LawggCrawlingApplication.java
@@ -0,0 +1,13 @@
+package com.kr.lg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LawggCrawlingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LawggCrawlingApplication.class, args);
+    }
+
+}
